a7f6fc8970ebfa3182e603e298db253ec42aabe9
Bram Crielaard <bram@crielaard.co.uk>
2019-06-22 15:41:11 +0200

    Add method to add noise to image

--- src/scene_change.py
+++ src/scene_change.py
@@ -1,4 +1,6 @@
 import argparse
+import numpy as np
+from skimage.util import random_noise
 
 
 def get_arguments():
@@ -11,8 +13,12 @@ def get_arguments():
 
 
 def scene_change(foreground, background):
-    
     pass
+
+
+def add_noise(image):
+    noisy = (255 * random_noise(image, mode='gaussian', var=0.0007)).astype(np.uint8)
+    return noisy
 
 
 if __name__ == "__main__":
