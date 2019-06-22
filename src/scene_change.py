8b9ae289c5a13d2454ff7e5c1328f3fa3b77a075
Gerben <g.j.w.oolbekkink@gmail.com>
2019-06-22 20:09:39 +0200

    Add position

--- src/scene_change.py
+++ src/scene_change.py
@@ -17,6 +17,42 @@ def scene_change(foreground, background):
     pass
 
 
+def position(foreground, fg_vp, background, bg_vp):
+    flip = True
+    x = 1
+    y = 2
+    scale = 1
+
+    (fg_h, fg_w) = foreground.shape[:2]
+    (bg_h, bg_w) = background.shape[:2]
+
+    assert fg_h <= bg_h
+    assert fg_w <= bg_w
+
+    (fg_vp_x, fg_vp_y) = fg_vp
+    (bg_vp_x, bg_vp_y) = bg_vp
+
+    # normal case no smart thing to do
+    scale = 1
+    flip = False
+
+    x = (bg_w - fg_w) / 2
+    y = bg_h - fg_h
+
+    # normalize vanishing points
+    # the foreground vanishing point expressed in the coordinates of the background
+
+    fg_vp_x_n = fg_vp_x + x
+    fg_vp_y_n = fg_vp_y + y
+
+    if fg_vp_y_n < bg_vp_y:
+        y = y + (bg_vp_y - fg_vp_y_n)
+
+    x = x + (bg_vp_x - fg_vp_x_n)
+
+    return [x, y], scale, flip
+
+
 def add_noise(image):
     """
     Add random noise to an image.
